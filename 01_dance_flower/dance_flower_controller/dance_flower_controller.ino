40ccd5b893dad11fda5b4b8c5f43d9bee3879c53
Michael Egger <me@anyma.ch>
2015-02-24 11:09:35 +0100

    4 sensors

--- 01_dance_flower/dance_flower_controller/dance_flower_controller.ino
+++ 01_dance_flower/dance_flower_controller/dance_flower_controller.ino
@@ -16,28 +16,23 @@
 #include "SPI.h"
 
 MIDIMessage message;        
-const int slaveSelectPin = 4;
 
-volatile unsigned char PdmCompare;
 
-unsigned char d[12];
 
-unsigned char sensorValue = 0;         // variable to store the value coming from the sensor
-unsigned char sentValue = 255;         // we only want to send data when there's something changing
+unsigned int sensorValue[4];         // variable to store the value coming from the sensor
+unsigned int sentValue[4];         // we only want to send data when there's something changing
                              // so we have to keep track of the last value that was sent to the host
+   unsigned char i;    
 
-
+ 
 //---------------------------------------------------------------------------------------------  //
 //																	SETUP
 //---------------------------------------------------------------------------------------------  //
 
 void setup() {               
- // set the slaveSelectPin as an output:
-  pinMode (slaveSelectPin, OUTPUT);
   // initialize SPI:
    Serial.begin(9600);
-
-    
+   
 }
 
 
@@ -46,15 +41,18 @@ void setup() {
 //---------------------------------------------------------------------------------------------  //
 
 void loop() {
-  unsigned char i;
   
-   sensorValue = analogRead(0) / 8;                       // analogRead returns 0-1023, we need 0-127
-  if (sensorValue != sentValue) {                         // compare actual readout to last sent value    
+  i++;
+  i %= 4;
+  
+  
+  //sensorValue[i] = ((sensorValue[i] * 3) + (analogRead(i) / 8)) / 4;                       // analogRead returns 0-1023, we need 0-127
+  
+   sensorValue[i] = analogRead(i) / 8;                       // analogRead returns 0-1023, we need 0-127
+  if (sensorValue[i] != sentValue[i]) {                         // compare actual readout to last sent value    
        
-      //MIDI.write(MIDI_CONTROLCHANGE, controller number , controller value )
-
-        MIDI.write(MIDI_CONTROLCHANGE,1,sensorValue);     // put new control change message into MIDI sending queue
-        sentValue = sensorValue;                          // store last sent value
+        MIDI.write(MIDI_CONTROLCHANGE,i,sensorValue[i]);     // put new control change message into MIDI sending queue
+        sentValue[i] = sensorValue[i];                          // store last sent value
   }
 
   
@@ -63,30 +61,13 @@ void loop() {
       
                                                          // don't forget the ampersand (&) before the variable name !
         if(message.command == MIDI_CONTROLCHANGE) {
-    /*  
-          digitalWrite(slaveSelectPin,LOW);
-            SPI.transfer(0xFF);
-            SPI.transfer(0x0F);
-            SPI.transfer(message.key);
-            SPI.transfer( 2 * message.value);
-            // take the SS pin high to de-select the chip:
-            digitalWrite(slaveSelectPin,HIGH); 
-                    */
-                  //  SPI.transfer(message.key);
               Serial.write(message.key);
               Serial.write(message.value*2);
-                  //  SPI.transfer(message.value);
-           // d[message.key%12] = message.value;
-
         }
 
         
     }    
   
-  
- // SPI.transfer(d[i]);
- // i++;
- // i%=12;
     delay(1);              // give some time for sending, otherwhise the MIDI queue could fill up
 }
 
